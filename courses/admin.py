a68608ecb36bb347be1f0181bb6403bbaf0dc725
Yusuf Hendricks <yhendricks1@gmail.com>
2017-10-17 09:50:36 +0200

    Initial commit

---
+++ courses/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from .models import Course, Text, Quiz
+
+
+class TextInline(admin.StackedInline):
+    model = Text
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [TextInline,]
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Text)
+admin.site.register(Quiz)
---
+++ courses/models.py
@@ -0,0 +1,48 @@
+from django.core.urlresolvers import reverse
+from django.db import models
+
+
+class Course(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Step(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    order = models.IntegerField(default=0)
+    course = models.ForeignKey(Course)
+
+    class Meta:
+        abstract = True
+        ordering = ['order',]
+
+    def __str__(self):
+        return self.title
+
+
+class Text(Step):
+    content = models.TextField(blank=True, default='')
+
+    def get_absolute_url(self):
+        return reverse('courses:text', kwargs={
+                'course_pk': self.course_id,
+                'step_pk': self.id
+        })
+
+
+class Quiz(Step):
+    total_questions = models.IntegerField(default=4)
+
+    class Meta:
+        verbose_name_plural = "Quizzes"
+
+    def get_absolute_url(self):
+        return reverse('courses:quiz', kwargs={
+                'course_pk': self.course_id,
+                'step_pk': self.id
+        })
